bc256e529c0e74d37331931547d5fabe5799d65a
Rebecca Nelson <tails.fanatic@gmail.com>
2015-09-18 21:14:24 -0500

    Added array and started pattern

---
+++ array.h
@@ -0,0 +1,12 @@
+#ifndef ARRAY_H_
+#define ARRAY_H_
+
+#include <stddet.h>
+
+typedef struct array_t
+{
+	void *ptr;
+	size_t size;
+} array_t;
+
+#endif
---
+++ pattern.h
@@ -0,0 +1,16 @@
+#ifndef PATTERN_H_
+#define PATTERN_H_
+
+#include <stdint.h>
+
+typedef struct pixel
+{
+	uint8_t red;
+	uint8_t blue;
+	uint8_t green;
+	uint8_t alpha;
+} pixel;
+
+int get_color(
+
+#endif
