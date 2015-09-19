577b12b73f9d131fc8bd86065a802ed7a60cb469
JPisaBrony <jdpohl789@gmail.com>
2015-09-19 06:10:02 -0500

    ^ initial image loading images are now being loaded properly

--- main3.c
+++ main3.c
@@ -2,10 +2,11 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+// event pooling
 SDL_Event event;
-
+// main window
 SDL_Window *window = NULL;
-
+// main screen
 SDL_Surface *screen = NULL;
 
 #define WINDOW_HEIGHT 600
@@ -14,61 +15,68 @@ SDL_Surface *screen = NULL;
 
 int init()
 {
-    // initilze sdl
+    // initilze SDL
     SDL_Init(SDL_INIT_EVERYTHING);
     
+    // create main window
     window = window = SDL_CreateWindow("Hack ISU", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
-
+    
+    // check pointers
     if(window == NULL)
         exit(1);
-
+    
+    // create main surface
     screen = SDL_GetWindowSurface(window);
-
+    
+    // check pointers
     if(screen == NULL)
         exit(2);
 }
 
 SDL_Surface* loadImage(char* filename)
 {
+    // optmized image
     SDL_Surface *opt_img = NULL;
-
-    int flags = IMG_INIT_PNG;
-
-    if(!IMG_Init(flags) & flags)
+    // image surface
+    SDL_Surface *img = IMG_Load(filename);
+    // check pointers
+    if(img == NULL)
     {
-        SDL_Surface *img = IMG_Load(filename);
-        if(img == NULL)
-        {
-            printf("can't load loadImage");
-            exit(3);
-        }
-        opt_img = SDL_ConvertSurface(img, screen->format, NULL);
-        SDL_FreeSurface(img);
+        printf("can't load loadImage");
+        exit(3);
     }
-    
+    // convert to screen format
+    opt_img = SDL_ConvertSurface(img, screen->format, NULL);
+    // free old surface
+    SDL_FreeSurface(img);
+    // return optimized img
     return opt_img;
 }
 
 int main()
 {
+    // initilize
     init();
 
-    SDL_Rect rect;
-    SDL_Surface* img = loadImage("twi.jpg");
-    rect.x = 0;
-    rect.y = 0;
-    SDL_BlitSurface(img, NULL, screen, &rect);
-
+    SDL_Surface* img = loadImage("twi.png");
+   
+    // while running
     while(1)
     {
+        // pool for events
         while(SDL_PollEvent(&event))
         {
+            // if window is closed
             if(event.type == SDL_QUIT)
             {
                 SDL_Quit();
                 return 0;
             }
         }
+        // apply image to screen
+        SDL_BlitSurface(img, NULL, screen, NULL);
+        // update screen
+        SDL_UpdateWindowSurface(window);
     }
 
     SDL_Quit();
