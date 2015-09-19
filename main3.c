d73fc9b11e9deda79f231eed3134f8764c1053ec
JPisaBrony <jdpohl789@gmail.com>
2015-09-19 08:47:49 -0500

    + pixel funcitons

--- main3.c
+++ main3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "image.h"
 
 // event pooling
 SDL_Event event;
@@ -35,8 +36,6 @@ int init()
 
 SDL_Surface* loadImage(char* filename)
 {
-    // optmized image
-    SDL_Surface *opt_img = NULL;
     // image surface
     SDL_Surface *img = IMG_Load(filename);
     // check pointers
@@ -45,6 +44,15 @@ SDL_Surface* loadImage(char* filename)
         printf("can't load loadImage");
         exit(3);
     }
+    return img;
+}
+
+SDL_Surface* loadImageForDisplay(char* filename)
+{
+    // optmized image
+    SDL_Surface *opt_img = NULL;
+    // load image
+    SDL_Surface *img = loadImage(filename);
     // convert to screen format
     opt_img = SDL_ConvertSurface(img, screen->format, NULL);
     // free old surface
@@ -53,13 +61,43 @@ SDL_Surface* loadImage(char* filename)
     return opt_img;
 }
 
+//
+struct image* createImageInfo(SDL_Surface *src)
+{
+    image_t *img = malloc(sizeof(image_t));
+    img->pixels = (Uint32*)src->pixels;
+    img->format = src->format;
+    img->bitdepth = src->format->BitsPerPixel;
+    img->width = src->w;
+    img->height = src->h;
+    return img;
+}
+
+
+struct pixel_t getPixelFromImage(struct image *img, int x, int y)
+{
+    pixel_t pix;
+    Uint32 *pixel = img->pixels;
+    int index = y * img->width + x;
+    pix.red = pixel[index] & (Uint8)img->format->Rmask;
+    pix.blue = pixel[index] & (Uint8)img->format->Gmask;
+    pix.green = pixel[index] & (Uint8)img->format->Bmask;
+    pix.alpha = pixel[index] & (Uint8)img->format->Amask;
+    return pix;
+}
+
 int main()
 {
     // initilize
     init();
 
-    SDL_Surface* img = loadImage("twi.png");
-   
+    SDL_Surface* img = loadImageForDisplay("twi.png");
+    struct image *img2 = createImageInfo(img);
+    
+    printf("Pixels: %d, BitDepth: %d, Width: %d, Height: %d\n", img2->pixels, img2->bitdepth, img2->width, img2->height);
+    struct pixel_t p = getPixelFromImage(img2, 0, 0);
+    printf("Red: %d, Blue: %d, Green: %d, Alpha: %d\n", p.red, p.blue, p.green, p.alpha);
+    
     // while running
     while(1)
     {
@@ -69,6 +107,7 @@ int main()
             // if window is closed
             if(event.type == SDL_QUIT)
             {
+                SDL_FreeSurface(img);
                 SDL_Quit();
                 return 0;
             }
